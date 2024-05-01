5f919f725658315567ec3db981f578002e4ce3ed
YuZhanHuang <bigjoe1992111@gmail.com>
2024-05-01 15:06:32 +0800

    Create Multiple List We made a big leap, moving from a single ShoppingItem model to multiple lists with multiple items. That meant changing the models, views, and serializers.
    Along the way, you worked with:
    - Nested serializers
    - Generic Views
    - Using lookup_url_kwarg to change the URL keyword argument for object lookup
    
    You also customized a serializer's default behavior by overriding the create method and adapting the Generic Views queryset by overriding the get_queryset method.

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.renderers import JSONRenderer
 
-from shopping_list.models import ShoppingItem
+from shopping_list.models import ShoppingItem, ShoppingList
 
 """
 DRF comes with a few serializer types out-of-the-box:
@@ -18,9 +18,6 @@ check this at http://127.0.0.1:8000/api/shopping-items/
 Since we actually don't want the client to be able to create their own UUIDs, 
 we can make the id field read-only.
 read-only reference: https://www.django-rest-framework.org/api-guide/fields/#read_only
-
-
-
 """
 
 
@@ -31,3 +28,31 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
         # Here is setting for specific serializer
         # renderer_classes = [JSONRenderer]
+
+    def create(self, validated_data, **kwargs):
+        """
+        This is the self.context['request'].parser_context
+        {
+            'view': <shopping_list.api.views.AddShoppingItem object at 0x10bb94c10>,
+            'args': (),
+            'kwargs': {
+                'pk': UUID('f0288d88-d7c5-4cf0-a89b-14c9bb7d0454')
+                },
+            'request':
+                <rest_framework.request.Request: POST '/api/shopping-lists/f0288d88-d7c5-4cf0-a89b-14c9bb7d0454/shopping-items/'>,
+            'encoding': 'utf-8'
+        }
+        :param validated_data:
+        :param kwargs:
+        :return:
+        """
+        validated_data['shopping_list_id'] = self.context['request'].parser_context['kwargs']['pk']
+        return super(ShoppingItemSerializer, self).create(validated_data)
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    shopping_items = ShoppingItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ShoppingList
+        fields = ['id', 'name', 'shopping_items']
